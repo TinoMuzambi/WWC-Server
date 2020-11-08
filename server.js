094f20f282e5dca3eb66df943baaaac56c77effc
Tino <tinomuzambi@gmail.com>
2020-11-08 16:19:32 +0200

    Switch clan route to request

--- server.js
+++ server.js
@@ -15,14 +15,6 @@ app.use(cors());
 // Routes
 app.get("/api/player/:id", (req, res) => {
 	const baseURL = "https://api.clashofclans.com/v1/players/%23" + req.params.id;
-			// const result = await fetch(baseURL, {
-			// 	headers: {
-			// 		"content-type": "application/json; charset=utf-8",
-			// 		Authorization: "Bearer " + process.env.CLASH_KEY,
-			// 	},
-			// });
-			// const data = await result.json();
-			// res.status(200).send(data);
 
 	const options = {
 		proxy: process.env.HTTP_PROXY,
@@ -34,10 +26,7 @@ app.get("/api/player/:id", (req, res) => {
 	}
 
 	const cb = (error, response, body) => {
-		console.log(response.statusCode);
-		console.log(body)
 		if (!error && response.statusCode == 200) {
-			console.log(body)
 			res.status(200).send(body)
 		}
 		else {
@@ -51,28 +40,28 @@ app.get("/api/player/:id", (req, res) => {
 });
 
 app.get("/api/clan/:id", (req, res) => {
-	const baseURL = "https://api.clashofclans.com/v1/clans/%23";
-	try {
-		const fetchData = async () => {
-			const clanID = req.params.id;
-			const result = await fetch(baseURL + clanID, {
-				headers: {
-					Accept: "*/*",
-					"content-type": "application/json; charset=utf-8",
-					Authorization: "Bearer " + process.env.CLASH_KEY,
-				},
-				agent: new HttpProxyAgent(process.env.FIXIE_URL),
-			});
+	const baseURL = "https://api.clashofclans.com/v1/clans/%23" + req.params.id;
 
-			const data = await result.json();
-			res.status(200).send(data);
-		};
-
-		fetchData();
-	} catch (error) {
-		console.error(error);
-		res.status(500).send(error);
+	const options = {
+		proxy: process.env.HTTP_PROXY,
+		url: baseURL,
+		headers: {
+			"content-type": "application/json; charset=utf-8",
+			Authorization: "Bearer " + process.env.CLASH_KEY,
+		}
 	}
+
+	const cb = (error, response, body) => {
+		if (!error && response.statusCode == 200) {
+			res.status(200).send(body)
+		}
+		else {
+			console.log(error);
+			res.status(500).send("Error yo")
+		}
+	}
+
+	request(options, cb);
 });
 
 app.get("/", (req, res) =>
